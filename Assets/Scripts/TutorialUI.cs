fd8f3c99c798038a123fee7a1ddd77bd296ea8bc
Nash <lostcloud1200@gmail.com>
2026-06-15 02:04:41 -0500

    tutorial

---
+++ Assets/Scripts/TutorialUI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TutorialUI : MonoBehaviour
+{
+    public GameObject tutorialPanel;
+    public float duration = 5f;
+
+    void Start()
+    {
+        tutorialPanel.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Invoke(nameof(HideTutorial), duration);
+    }
+
+    void HideTutorial()
+    {
+        tutorialPanel.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
